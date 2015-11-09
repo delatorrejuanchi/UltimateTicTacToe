f5bda7365ef30514ad61a8a9e9e370b476befe4f
Juan Cruz de La Torre <delatorrejuanchi@gmail.com>
2015-11-08 17:05:18 -0800

    update

---
+++ README.md
@@ -0,0 +1,78 @@
+# UltimateTicTacToe
+
+TicTacToe is a classic, but as you may have discovered is yourself, it is terminally dull. There's no room for creativity or insight, since good players always tie.
+
+For this reason, a more sophisticated version was invented, which I decided to implement this variation using web technologies.
+
+## Rules
+
+<p align="center">
+    At first, the board looks like this:
+</p>
+
+<p align="center">
+    <img src="images/blank-board.jpg" alt="">
+</p>
+
+<p align="center">
+    Each turn, you mark one of the small squares.
+</p>
+
+<p align="center">
+    <img src="images/early-game.jpg" alt="">
+</p>
+
+<p align="center">
+    When you get three in a row on a small board, you've won that board.
+</p>
+
+<p align="center">
+    <img src="images/mid-game.jpg" alt="">
+</p>
+
+<p align="center">
+    To win the game, you need to win three small boards in a row.
+</p>
+
+
+<p align="center">
+    <img src="images/end-game.jpg" alt="">
+</p>
+
+### But however, the most important rule is:
+**You don't get to pick which of the nine boards to play on.** That's determined by your opponent's previous move. **Whichever _square_ he picks, that's the board you must play in next.** (And whichever square _you_ pick will determine which board _he_ plays on next)
+
+
+#### This way, if I play here...
+
+
+![alt tag](images/first-move.jpg)
+
+
+#### Then your next move must be here...
+
+
+![alt tag](images/second-move.jpg)
+
+
+This lends the game a strategic element. You can’t just focus on the little board. You’ve got to consider where your move will send your opponent, and where his next move will send you, and so on.
+
+
+**The resulting scenarios look bizarre**. Players seem to move randomly, missing easy two- and three-in-a-rows. But there’s a method to the madness – they’re thinking ahead to future moves, wary of setting up their opponent on prime real estate. It is, in short, vastly more interesting than regular TicTacToe.
+
+
+<p align="center">
+    <img src="images/late-game.jpg" alt="">
+</p>
+
+
+### A few clarifying rules are necessary:
+- _What if my opponent sends me to a board that's already been won?_ In that case, congratulations - you get to go anywhere you like, on any of the other boards. (This means you should avoid sending your opponent to an already-won board!)
+
+- _What if one of the small boards results in a tie?_ Well, then the board counts for neither X (red) nor O (blue).
+
+## That's it. Have fun!
+
+You can find a [demo] here.
+
+[demo]: http://delatorrejuanchi.github.io/UltimateTicTacToe
---
+++ main.js
@@ -0,0 +1,189 @@
+$('document').ready(function () {
+    function TicTacToe() {
+        this.board = [
+            ['-', '-', '-'],
+            ['-', '-', '-'],
+            ['-', '-', '-']
+        ];
+    }
+    TicTacToe.prototype.get_cell = function (cell) {
+        cell--;
+        var x = cell % 3;
+        var y = Math.floor(cell / 3);
+
+        return this.board[y][x];
+    }
+    TicTacToe.prototype.is_valid_play = function (cell) {
+        return this.get_cell(cell) == '-';
+    }
+    TicTacToe.prototype.play = function (player, cell) {
+        var x, y;
+        cell = cell - 1;
+        y = Math.floor(cell / 3);
+        x = cell % 3;
+
+        this.board[y][x] = player;
+    }
+    TicTacToe.prototype.is_won = function () {
+        if (this.get_cell(1) == this.get_cell(2) && this.get_cell(2) == this.get_cell(3) && this.get_cell(1) != "-" && this.get_cell(2) != "-" && this.get_cell(3) != "-") {
+            return this.get_cell(1);
+        }
+        if (this.get_cell(4) == this.get_cell(5) && this.get_cell(5) == this.get_cell(6) && this.get_cell(4) != "-" && this.get_cell(5) != "-" && this.get_cell(6) != "-") {
+            return this.get_cell(4);
+        }
+        if (this.get_cell(7) == this.get_cell(8) && this.get_cell(8) == this.get_cell(9) && this.get_cell(7) != "-" && this.get_cell(8) != "-" && this.get_cell(9) != "-") {
+            return this.get_cell(7);
+        }
+        if (this.get_cell(1) == this.get_cell(4) && this.get_cell(4) == this.get_cell(7) && this.get_cell(1) != "-" && this.get_cell(4) != "-" && this.get_cell(7) != "-") {
+            return this.get_cell(1);
+        }
+        if (this.get_cell(2) == this.get_cell(5) && this.get_cell(5) == this.get_cell(8) && this.get_cell(2) != "-" && this.get_cell(5) != "-" && this.get_cell(8) != "-") {
+            return this.get_cell(2);
+        }
+        if (this.get_cell(3) == this.get_cell(6) && this.get_cell(6) == this.get_cell(9) && this.get_cell(3) != "-" && this.get_cell(6) != "-" && this.get_cell(9) != "-") {
+            return this.get_cell(3);
+        }
+        if (this.get_cell(1) == this.get_cell(5) && this.get_cell(5) == this.get_cell(9) && this.get_cell(1) != "-" && this.get_cell(5) != "-" && this.get_cell(9) != "-") {
+            return this.get_cell(1);
+        }
+        if (this.get_cell(7) == this.get_cell(5) && this.get_cell(5) == this.get_cell(3) && this.get_cell(7) != "-" && this.get_cell(5) != "-" && this.get_cell(3) != "-") {
+            return this.get_cell(7);
+        }
+
+        var playable = false;
+        for (var cell = 1; cell < 10; cell++) {
+            if (this.get_cell(cell).indexOf("-") > -1) {
+                playable = true;
+            }
+        }
+        if (!playable) {
+            return "DRAW";
+        }
+
+        return false;
+    }
+
+    function UltimateTicTacToe() {
+        this.boards = [
+            [new TicTacToe(), new TicTacToe(), new TicTacToe()],
+            [new TicTacToe(), new TicTacToe(), new TicTacToe()],
+            [new TicTacToe(), new TicTacToe(), new TicTacToe()]
+        ];
+        this.next_board = null;
+    }
+    UltimateTicTacToe.prototype.get_board = function (board) {
+        board--;
+        var x = board % 3;
+        var y = Math.floor(board / 3);
+
+        return this.boards[y][x];
+    }
+    UltimateTicTacToe.prototype.is_valid_play = function (board, cell) {
+        if (this.get_board(board) != this.next_board && this.next_board != null) {
+            return false;
+        }
+        if (!this.get_board(board).is_valid_play(cell)) {
+            return false;
+        }
+        if (this.get_board(board).is_won()) {
+            return false;
+        }
+
+        return true
+    }
+    UltimateTicTacToe.prototype.play = function (player, board, cell) {
+        this.get_board(board).play(player, cell);
+        this.next_board = this.get_board(cell);
+        if (this.next_board.is_won()) {
+            this.next_board = null;
+        }
+    }
+    UltimateTicTacToe.prototype.is_won = function () {
+        if (this.get_board(1).is_won() == this.get_board(2).is_won() && this.get_board(2).is_won() == this.get_board(3).is_won() && this.get_board(1).is_won() != "DRAW" && this.get_board(2).is_won() != "DRAW" && this.get_board(3).is_won() != "DRAW" && this.get_board(1).is_won() != false && this.get_board(2).is_won() != false && this.get_board(3).is_won() != false) {
+            return this.get_board(1).is_won();
+        }
+        if (this.get_board(4).is_won() == this.get_board(5).is_won() && this.get_board(5).is_won() == this.get_board(6).is_won() && this.get_board(4).is_won() != "DRAW" && this.get_board(5).is_won() != "DRAW" && this.get_board(6).is_won() != "DRAW" && this.get_board(4).is_won() != false && this.get_board(5).is_won() != false && this.get_board(6).is_won() != false) {
+            return this.get_board(4).is_won();
+        }
+        if (this.get_board(7).is_won() == this.get_board(8).is_won() && this.get_board(8).is_won() == this.get_board(9).is_won() && this.get_board(7).is_won() != "DRAW" && this.get_board(8).is_won() != "DRAW" && this.get_board(9).is_won() != "DRAW" && this.get_board(7).is_won() != false && this.get_board(8).is_won() != false && this.get_board(9).is_won() != false) {
+            return this.get_board(7).is_won();
+        }
+        if (this.get_board(1).is_won() == this.get_board(4).is_won() && this.get_board(4).is_won() == this.get_board(7).is_won() && this.get_board(1).is_won() != "DRAW" && this.get_board(4).is_won() != "DRAW" && this.get_board(7).is_won() != "DRAW" && this.get_board(1).is_won() != false && this.get_board(4).is_won() != false && this.get_board(7).is_won() != false) {
+            return this.get_board(1).is_won();
+        }
+        if (this.get_board(2).is_won() == this.get_board(5).is_won() && this.get_board(5).is_won() == this.get_board(8).is_won() && this.get_board(2).is_won() != "DRAW" && this.get_board(5).is_won() != "DRAW" && this.get_board(8).is_won() != "DRAW" && this.get_board(2).is_won() != false && this.get_board(5).is_won() != false && this.get_board(8).is_won() != false) {
+            return this.get_board(2).is_won();
+        }
+        if (this.get_board(3).is_won() == this.get_board(6).is_won() && this.get_board(6).is_won() == this.get_board(9).is_won() && this.get_board(3).is_won() != "DRAW" && this.get_board(6).is_won() != "DRAW" && this.get_board(9).is_won() != "DRAW" && this.get_board(3).is_won() != false && this.get_board(6).is_won() != false && this.get_board(9).is_won() != false) {
+            return this.get_board(3).is_won();
+        }
+        if (this.get_board(1).is_won() == this.get_board(5).is_won() && this.get_board(5).is_won() == this.get_board(9).is_won() && this.get_board(1).is_won() != "DRAW" && this.get_board(5).is_won() != "DRAW" && this.get_board(9).is_won() != "DRAW" && this.get_board(1).is_won() != false && this.get_board(5).is_won() != false && this.get_board(9).is_won() != false) {
+            return this.get_board(1).is_won();
+        }
+        if (this.get_board(7).is_won() == this.get_board(5).is_won() && this.get_board(5).is_won() == this.get_board(3).is_won() && this.get_board(7).is_won() != "DRAW" && this.get_board(5).is_won() != "DRAW" && this.get_board(3).is_won() != "DRAW" && this.get_board(7).is_won() != false && this.get_board(5).is_won() != false && this.get_board(3).is_won() != false) {
+            return this.get_board(7).is_won();
+        }
+
+        var ended = 0;
+        for (var board = 1; board < 10; board++) {
+            ended += (this.get_board(board).is_won()) ? 1 : 0;
+        }
+        if (ended == 9) {
+            return "DRAW";
+        }
+
+        return false;
+    }
+
+    var game = new UltimateTicTacToe();
+    var current_player = "X";
+    $('.game').addClass("X-TURN");
+
+    $('.cell').click(function (event) {
+        event.preventDefault();
+
+        link = $(this).attr('href').split("-");
+
+        var board = parseInt(link[0]);
+        var cell = parseInt(link[1]);
+
+        if (game.is_valid_play(board, cell)) {
+            game.play(current_player, board, cell);
+            $(this).addClass(current_player);
+            current_player = current_player == "X" ? "O" : "X";
+            
+            $('.next_board').removeClass('next_board');
+            if(!game.get_board(cell).is_won()) {
+                $('.board:nth-of-type(' + (cell) + ')').addClass('next_board');
+            }
+        }
+        
+        if(game.get_board(board).is_won()) {
+            var won = game.get_board(board).is_won();
+            $(this).parent(".board").addClass(won);
+            $(this).parent(".board").children(".cell")
+                .removeClass("X")
+                .removeClass("O")
+                .removeClass("DRAW")
+                .addClass(won);
+        }
+        
+        if (game.is_won()) {
+            alert(game.is_won());
+            game = new UltimateTicTacToe();
+            $('.X').removeClass('X')
+            $('.O').removeClass('O');
+            $('.DRAW').removeClass('DRAW');
+            current_player = "X";
+        }
+        
+        if(current_player == "X") {
+            $('.game').removeClass("O-TURN").addClass("X-TURN");
+        } else {
+            $('.game').removeClass("X-TURN").addClass("O-TURN");
+
+        }
+        
+        return true;
+    });
+});
